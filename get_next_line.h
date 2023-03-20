ab71459e41ebbc69b7c0d2308ccdf81d677ffe9a
Muhammed Can Kırkgöz <mcankirkgoz@gmail.com>
2023-03-20 20:13:41 +0300

    msg

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkirkgoz <mkirkgoz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/29 01:30:33 by mkirkgoz          #+#    #+#             */
+/*   Updated: 2023/03/17 23:17:54 by mkirkgoz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *string);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+#endif
